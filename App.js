3bc832634b0a8081b832ac47b7e7b4071e4287de
AsadUllahShaban <asadullahshaban1997@gmail.com>
2023-02-16 08:24:38 +0500

    Initial homeScreen ready

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider, provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 export default function App() {
     return (
         <Provider store={store}>
-            <View style={styles.container}>
-                <Text>Let Dive into Uber Clone App!</Text>
-                <StatusBar style="auto" />
-            </View>
+            <HomeScreen />
         </Provider>
     );
 }
